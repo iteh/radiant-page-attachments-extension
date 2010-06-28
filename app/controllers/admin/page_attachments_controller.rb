6d644ce39d1033a79d4238f739f8fbff40ae734a
johnmuhl <git@johnmuhl.com>
2010-06-28 18:14:36 -0500

    get pagination working with 0.9

--- app/controllers/admin/page_attachments_controller.rb
+++ app/controllers/admin/page_attachments_controller.rb
@@ -1,13 +1,14 @@
 class Admin::PageAttachmentsController < Admin::ResourceController
-  
+  paginate_models
+
   def index
     collection = self.respond_to?(:current_site) ? current_site.page_attachments : PageAttachment
-    @attachments = collection.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(@pagination_parameters)
+    @attachments = collection.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename')
   end
   
   def grid
     collection = self.respond_to?(:current_site) ? current_site.page_attachments : PageAttachment
-    @attachments = collection.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(@pagination_parameters)
+    @attachments = collection.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename')
   end
   
   def update
